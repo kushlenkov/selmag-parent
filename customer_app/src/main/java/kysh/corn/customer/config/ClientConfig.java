b4df9b48b2c9054bcf67ea4ee26c8365c979f3d5
kushlenkov <kushlenkov.av@gmail.com>
2025-03-23 16:24:06 +0300

    feat: SC24EP16 step 1

--- customer_app/src/main/java/kysh/corn/customer/config/ClientConfig.java
+++ customer_app/src/main/java/kysh/corn/customer/config/ClientConfig.java
@@ -8,6 +8,7 @@ import de.codecentric.boot.admin.client.config.ClientProperties;
 import de.codecentric.boot.admin.client.registration.ReactiveRegistrationClient;
 import de.codecentric.boot.admin.client.registration.RegistrationClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -23,6 +24,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ClientConfig {
 
     @Bean
+    @LoadBalanced
     @Scope("prototype")
     public WebClient.Builder selmagServicesWebClientBuilder(
             ReactiveClientRegistrationRepository clientRegistrationRepository,
